d2288d6bdd0a320402979827aec364af42d9247d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-11 19:48:49 -0500

    first commit

---
+++ Learnin.py
@@ -0,0 +1,55 @@
+#basic print--------------------------------------
+
+#print ('I like pizza!')
+#print ("It's really good!")
+
+#all variables------------------------------------
+
+#first_name = 'Riley'
+#last_name = 'Fowler'
+#full_name = first_name +' '+ last_name
+#print ('hello '+full_name)
+#print (type(full_name))
+
+#age = 20
+#age += 1
+#print('your age is ' +str(age))
+#print(type(age))
+#height = 250.5
+#print ('your height is '+str(height)+'cm')
+#print (type(height))
+
+#human = True
+#print (human)
+#print (type(human))
+#print('are you a human:'+str(human))
+
+#Multiple assignment------------------------------
+
+#name, age, attractive = 'Riley', 20, False
+
+#print ('Name:' +str (name))
+#print ('Age:'+str (age))
+#print ('Are they attractive?: ' +str (attractive))
+
+#Spongebob = Patrick = Squidward = Sandy = 30
+#print (Spongebob)
+#print (Patrick)
+#print (Squidward)
+#print (Sandy)
+
+#string methods------------------------------------
+
+#name = 'riLey'
+#print (len(name))
+#print (name.find('i'))
+#print (name.capitalize())
+#print (name.upper())
+#print (name.lower())
+#print (name.isdigit())
+#print (name.isalpha())
+#print(name.count('r'))
+#print (name.replace('r','y'))
+#print (name*5)
+
+#
