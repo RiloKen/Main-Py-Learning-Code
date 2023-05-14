74f8b20e7ddccc1585ffa9992f6ca69606f10e6d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-14 16:36:14 -0500

    first commit

--- Learnin.py
+++ Learnin.py
@@ -52,4 +52,138 @@
 #print (name.replace('r','y'))
 #print (name*5)
 
-#
+#type casting-------------------------------------
+
+#x = 1   #int
+#y = 2.0 #float
+#z = '3' #str
+
+#x = float(x)
+#y = int(y)
+#z = int(z)
+
+#print(x)
+#print(y)
+#print(z)
+
+#print(z*3)
+
+#user input basic---------------------------------------
+
+#name = input("What is your name?: ")
+#print("Hello " +name)
+
+#age = input("How old are you?: ")
+#print(("You are " +age)+" years old")
+
+#inputs with type casting below////////////////////////
+
+#age = int(input("How old are you?: "))
+#age = age + 1
+#print("Next year you will be "+str(age)+" years old.")
+
+#weight = float(input('How much do you weigh in pounds,exactly? '))
+#weight = weight - 20
+#print("Wowza! Lay off those cupcakes, and maybe you could weigh "+str(weight)+" pounds!")
+
+#height = int(input('Okay, Okay. One last question...how tall are you in inches?: '))
+#print(str(height) + " inches" + "...dang, look at this midget! Is your last name Gaylord?!?!")
+
+#math functions----------------------------------------
+
+#pi = 3.14
+#import math
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#print(pow(pi,2))
+#print(math.sqrt(pi))
+
+#pi = -3.14
+#print(abs(pi))
+
+#x = 1
+#y = 2
+#z = 3
+
+#print(max(x,y,z))
+#print(min(x,y,z))
+
+#string slicing-----------------------------------------
+
+#name = "Riley Fowler"
+
+#first_name = name[0] #starting index
+#first_name = name[0:5]  # also [:5] #stopping index
+#last_name = name[6:12]  # also [6:] #substring
+#funky_name = name[0:12:2]  # also [::2] #step index
+#reversed_name = name[::-1]  # reverse a string
+
+#print(first_name)
+#print(last_name)
+#print(funky_name)
+#print(reversed_name)
+
+#slice function////////////////////////////////////////
+
+#website1 = "http://google.com"
+#website2 = "http://wikipedia.com"
+
+#slice = slice(7,-4)
+
+#print (website1[slice])
+#print (website2[slice])
+
+#if/elif/else statements------------------------------------------
+
+#age = int(input("How old are you?: "))
+
+#if age == 100:
+    #print("You're a century old!")
+#elif age >= 18:
+    #print("You're an adult!")
+#elif age < 0:
+    #print("You haven't been born yet!")
+#else:
+    #print("You're a minor!")
+
+#logical operators (and,or,not)------------------------------------
+
+#temp = int(input("What is the temperature outside? "))
+
+#if temp >= 32 and temp <= 86:
+    #print("The temperature is good today!")
+    #print("Go outside!")
+#elif temp < 32 or temp > 86:
+    #print("The temperature is bad today!")
+    #print("Stay inside!")
+
+#if not (temp >= 32 and temp <= 86):
+    #print("The temperature is bad today!")
+    #print("Stay inside!")
+#elif not (temp < 32 or temp > 86):
+    #print("The temperature is good today!")
+    #print("Go outside!")
+
+#while loops-------------------------------------------------------
+
+#while 1==1:
+    #print("Help! I'm stuck in an infinite loop!")
+
+#name = ""
+#while len(name) == 0:
+    #name = input("Enter your name: ")
+
+#print("Hello "+name)
+
+#another way to write it//////////////////////////////////////////
+
+#name = None
+
+#while not name:
+    #name = input("Enter your name: ")
+
+#print("Hello "+name)
+
+#for loops-------------------------------------------------------
