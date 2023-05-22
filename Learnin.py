705a9f24176d6eb3aa4aab03740de6e85c0c82ba
BuildTools <unconfigured@null.spigotmc.org>
2023-05-22 18:28:30 -0500

    changes

--- Learnin.py
+++ Learnin.py
@@ -187,3 +187,56 @@
 #print("Hello "+name)
 
 #for loops-------------------------------------------------------
+
+#for i in range(10):
+    #print(i+1)
+
+#for i in range(50,100+1,2):
+    #print(i)
+
+#for i in "Riley Fowler":
+    #print(i)
+
+#import time
+
+#for seconds in range(10,0,-1): #doesn't have to be "seconds" can be "i" or anything
+    #print(seconds)
+    #time.sleep(1)
+#print("Countdown Complete!")
+
+#nested loops------------------------------------------------------
+
+#rows = int(input("How many rows?: "))
+#columns = int(input("How many columns?: "))
+#symbol = input("Enter a symbol to use: ")
+
+#for i in range(rows):
+    #for j in range(columns):
+        #print(symbol, end= "")
+    #print()
+
+#loop control statements--------------------------------------------
+#break = used to terminate the loop entirely.
+#continue = skips to the next iteration of the loop.
+#pass = does nothing, acts as a placeholder.
+
+while True:
+    name = input("Enter your name: ")
+    if name != "":
+        break
+
+phone_number = "123-456-7890"
+
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="")
+
+for i in range(1,21):
+
+    if i == 13:
+        pass
+    else:
+        print(i)
+
+#lists---------------------------------------------------
